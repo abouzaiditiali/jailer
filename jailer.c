0ef5ff8759de880100d9197b137f971f7cb03211
Aymane Bouzaidi Tiali <aymanebouzaiditiali@gmail.com>
2026-08-06 06:11:41 +0000

    error handling

--- jailer.c
+++ jailer.c
@@ -8,27 +8,60 @@
 
 int create_cgroup(int jail_id) {
     char path[PATH_MAX];
-    snprintf(path, sizeof(path), "/sys/fs/cgroup/system.slice/quickC.slice/runtime.service/jails/jail-%d", jail_id);
-
-    mkdir(path, 0755);
-
     char memory_path[PATH_MAX];
     char pids_path[PATH_MAX];
     char cpu_path[PATH_MAX];
 
-    snprintf(memory_path, sizeof(memory_path), "%s/memory.max", path);
-    snprintf(pids_path, sizeof(pids_path), "%s/pids.max", path);
-    snprintf(cpu_path, sizeof(cpu_path), "%s/cpu.max", path);
+    /* build paths for cgroup and resource controllers */
+    if (snprintf(path, sizeof(path), "/sys/fs/cgroup/system.slice/quickC.slice/runtime.service/jails/jail-%d", jail_id) < 0) {
+        perror("snprintf path create_cgroup");
+        return -1;
+    }
 
-    //configure cgroup limits
-    write_file(memory_path, "268435456");
-    write_file(pids_path, "32");
-    write_file(cpu_path, "50000 100000");
+    if (snprintf(memory_path, sizeof(memory_path), "%s/memory.max", path) < 0) {
+        perror("snprintf memory_path create_cgroup");
+        return -1;
+    }
+    if (snprintf(pids_path, sizeof(pids_path), "%s/pids.max", path) < 0) {
+        perror("snprintf pids_path create_cgroup");
+        return -1;
+    }
+    if (snprintf(cpu_path, sizeof(cpu_path), "%s/cpu.max", path) < 0) {
+        perror("snprintf cpu_path create_cgroup");
+        return -1;
+    }
+
+    /* create cgroup directory for the current jail */
+    if (mkdir(path, 0755) == -1) {
+        perror("mkdir path create_cgroup");
+        return -1;
+    }
+
+    /* configure cgroup limits */
+    //64 × 1024 × 1024 = 67,108,864 bytes
+    if (write_file(memory_path, "67108864") == -1) {
+        perror("write_file memory_path create_cgroup");
+        return -1;
+    }
+    //number of tasks allowed (threads)
+    if (write_file(pids_path, "1") == -1) {
+        perror("write_file pids_path create_cgroup");
+        return -1;
+    }
+    //For every 100,000 microseconds = 100 ms, the tasks in this cgroup are 
+    //allowed to run for a total of 50,000 microseconds = 50 ms
+    if (write_file(cpu_path, "50000 100000") == -1) {
+        perror("write_file cpu_path create_cgroup");
+        return -1;
+    }
     
     int cgfd = open(path, O_RDONLY | O_DIRECTORY | O_CLOEXEC);
+    //set close-on-exec flag so child program doesn't inherit cgfd on exec
     if (cgfd == -1) {
-        perror("open cgroup");
-        rmdir(path);
+        perror("open path create_cgroup");
+        if (rmdir(path) == -1) {
+            perror("rmdir path create_cgroup");
+        }
         return -1;
     }
 
@@ -38,36 +71,58 @@ int create_cgroup(int jail_id) {
 int setup_rootfs(int jail_id) {
     /* change propagation type to private over whole tree */
     if (mount(NULL, "/", NULL, MS_REC | MS_PRIVATE, NULL) == -1) {
-        perror("mount propagation failed");
+        perror("mount propagation setup_rootfs");
         return -1;
     }
 
-    /* locate rootfs for the specific jail */
+    /* locate rootfs for the current jail */
     char rootfs[PATH_MAX];
-    snprintf(rootfs, sizeof(rootfs), "/var/lib/quickC/rootfs/jail-%d", jail_id);
+    if (snprintf(rootfs, sizeof(rootfs), "/var/lib/quickC/rootfs/jail-%d", jail_id) < 0) {
+        perror("snprintf rootfs setup_rootfs");
+        return -1;
+    }
     
     /* new root must be a mount point so we bind mount it onto itself */
     if (mount(rootfs, rootfs, NULL, MS_BIND | MS_REC, NULL)) {
-        perror("bind mount failed");
+        perror("mount bind setup_rootfs");
         return -1;
     }
     
     char oldroot[PATH_MAX];
-    snprintf(oldroot, sizeof(oldroot), "/var/lib/quickC/rootfs/jail-%d/oldroot", jail_id);
+    if (snprintf(oldroot, sizeof(oldroot), "/var/lib/quickC/rootfs/jail-%d/oldroot", jail_id) < 0) {
+        perror("snprintf oldroot setup_rootfs");
+        return -1;
+    }
 
-    mkdir(oldroot, 0755);
+    if (mkdir(oldroot, 0755) == -1) {
+        perror("mkdir oldroot setup_rootfs");   
+        return -1;
+    }
 
-    /* change root mount in the mount namespace of the calling process */
-    syscall(SYS_pivot_root, rootfs, oldroot);
-    chdir("/");
+    /* change root mount in the mount namespace of the current jail */
+    if (syscall(SYS_pivot_root, rootfs, oldroot) == -1) {
+        perror("SYS_pivot_root setup_rootfs");
+        return -1;
+    }
+    if (chdir("/") == -1) {
+        perror("chdir setup_rootfs");
+        return -1;
+    }
     
-    umount2("/oldroot", MNT_DETACH);
-    rmdir("/oldroot");
+    if (umount2("/oldroot", MNT_DETACH) == -1) {
+        perror("umount2 oldroot setup_rootfs");
+        return -1;
+    }
+
+    if (rmdir("/oldroot") == -1) {
+        perror("rmdir oldroot setup_rootfs");
+        return -1;
+    }
 
     return 0;    
 }
 
-int setup_clargs(struct clone_args *cl_args) {
+int setup_clone_arguments(struct clone_args *cl_args) {
     cl_args->exit_signal = SIGCHLD;
     cl_args->flags = CLONE_INTO_CGROUP | 
                     CLONE_NEWIPC | 
@@ -79,53 +134,69 @@ int setup_clargs(struct clone_args *cl_args) {
 
     int cgfd = create_cgroup(jail_id);
     if (cgfd == -1) {
-        perror("create cgroup failed");
         return -1;
     }
     cl_args->cgroup = cgfd; //child never runs outside specified cgroup
     return 0;
 }
 
+int validate_arguments(int argc, char* argv[]) {
+    if (argc != 2) { 
+        fprintf(stderr, "Usage: %s <jail_id>\n", argv[0]);
+        return -1;
+    }
+    char *endptr; //endptr points at the character immediately after the number 
+    long jail_id = strtol(argv[1], &endptr, 10); //10 is base
+    
+    //handle more error cases, for now input is controlled
+    return jail_id; 
+}
 
 int main(int argc, char* argv[]) {
-    if (validate_arguments(argc, argv) 
-    int jail_id = argv[1];
-
-    /* setup clone arguments */
-    struct clone_args cl_args = {0};
-    if (setup_clargs(&cl_args) == -1) {
-        perror("setup clargs");
+    /* extract arguments */
+    int jail_id = extract_argument(argc, argv);
+    if (jail_id == -1) {
         exit(EXIT_FAILURE);
     }
+
+    /* setup clone arguments + create cgroup */
+    struct clone_args cl_args = {0};
+    if (setup_clone_arguments(&cl_args) == -1) {
+        exit(EXIT_FAILURE);
+    }
+    //new cgroup directory created for current jail at path + open cgfd 
+    //make sure to delete directory at path. cgfd will be closed on child exec
 
     /* call clone3 */
     pid_t pid = syscall(SYS_clone3, &cl_args, sizeof(cl_args));
     if (pid == -1) {
-        perror("clone3");
+        perror("SYS_clone3 main");
         exit(EXIT_FAILURE);
     }
 
     if (pid == 0) {
-        /*
-        --Child inherits copy of file descriptor table. Both parent and child
-        tables point to the same open files.
-        */
-        /* Child needs a mounted directory that can become its new root */
+        /* private propagation + bind mount + pivot_root */
         if (setup_rootfs(jail_id) == -1) {
             exit(EXIT_FAILURE);
         }
         
         char exec_path[PATH_MAX];
-        snprintf(exec_path, sizeof(exec_path), "/var/lib/quickC/rootfs/jail-%d/program", jail_id);
+        if (snprintf(exec_path, sizeof(exec_path), "/var/lib/quickC/rootfs/jail-%d/program", jail_id) < 0) {
+            perror("snprintf exec_path main");
+            exit(EXIT_FAILURE);
+        }
         execve(exec_path);
 
         //execve failed
         perror("execve");
-        exit(EXIT_FAILURE).
+        _exit(EXIT_FAILURE).
     }
 
     int status;
-    waitpid(pid, &status, 0);
+    if (waitpid(pid, &status, 0) == -1) {
+        perror("waitpid main");
+        exit(EXIT_FAILURE);
+    }
     if (WIFEXITED(status)) {
         //program succeeded or execve failed
         printf("Exited with code %d\n", WEXITSTATUS(status));
